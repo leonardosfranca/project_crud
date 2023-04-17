f50aa2e84102e57fd50adb493c047aee464c270d
leonardo <leonardo5155@gmail.com>
2023-04-17 13:53:51 -0300

    primeiro commit

--- src/main/java/com/project/controller/ProdutoController.java
+++ src/main/java/com/project/controller/ProdutoController.java
@@ -1,56 +1,40 @@
 package com.project.controller;
 
 import com.project.entity.Produto;
-import com.project.exception.ProdutoNaoEncontradoException;
 import com.project.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/p1")
 public class ProdutoController {
     @Autowired
-    ProdutoService service;
+    ProdutoService produtoServiceservice;
 
     @PostMapping("/salvar")
-    public ResponseEntity<Produto>criarProduto(@RequestBody Produto produto){
-        return ResponseEntity.ok(service.criarProduto(produto));
+    public Produto salvarProduto(@RequestBody Produto produto){
+        return produtoServiceservice.salvarProduto(produto);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Produto> buscarProdutoId(@PathVariable("id") Long id) {
-        try {
-            Produto produto = service.buscarProdutoId(id);
-            return ResponseEntity.ok(produto);
-        } catch (ProdutoNaoEncontradoException e) {
-            return ResponseEntity.notFound().build();
-        }
+    public Produto buscarProdutoId(@PathVariable("id") Long id) {
+        return produtoServiceservice.buscarProdutoId(id);
     }
     @DeleteMapping("/deletar/{id}")
-    public ResponseEntity<Void> exlcuirProduto(@PathVariable("id") Long id){
-
-        try {
-            service.excluirProdutoPorId(id);
-            return ResponseEntity.noContent().build();
-        } catch (ProdutoNaoEncontradoException e) {
-            return ResponseEntity.notFound().build();
-        }
+    public void exlcuirProduto(@PathVariable("id") Long id){
+        produtoServiceservice.excluirProdutoPorId(id);
     }
 
     @PutMapping("/atualizar/{id}")
-    public ResponseEntity<Produto>atualizar(@PathVariable Long id, @RequestBody Produto produtoAtualizado){
-        return ResponseEntity.ok(service.atualizar(produtoAtualizado));
+    public Produto atualizar(@PathVariable Long id, @RequestBody Produto produtoAtualizado){
+        return produtoServiceservice.atualizar(produtoAtualizado);
     }
 
     @GetMapping("/produtos")
-    public ResponseEntity<?> listarProdutos() {
-        List<Produto> produtos = service.listarProdutos();
-        return ResponseEntity.ok(produtos);
+    public List<Produto> listarProdutos() {
+        return (produtoServiceservice.listarProdutos());
     }
-
 
 }
